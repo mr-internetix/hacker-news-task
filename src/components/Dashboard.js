ae23a14403295e1c4ab8c245d0888dcacecfca4d
Ajit Yadav <yadavajit8916@gmail.com>
2022-09-08 09:35:56 +0530

    make it better

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -15,7 +15,7 @@ function Dashboard() {
          <div style={{background: "#6a11cb"}}>
 		
         <h1 className="fw-bold mb-2 text-uppercase text-white">Hacker News WebApp</h1>
-        <button className="btn btn-lg px-5 bg-dark mb-4 text-white" >Logout</button> <br></br>
+        <button className="btn btn-lg px-5 bg-dark mb-4 text-white" onClick={handleLogout}>Logout</button> <br></br>
          <Link  className="w-bold mb-2 text-uppercase text-white" to='/favourites'>See Your Bookmarks Here! </Link>
         <div className="row">
         <Stories style={{background: "#6a11cb"}}/>
